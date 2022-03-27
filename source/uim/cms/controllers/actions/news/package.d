c1de3d9ec1c28a703894dbfd859b1e0bd223cf28
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-03-27 08:44:53 +0200

    Using EntityActions

--- source/uim/cms/controllers/actions/news/package.d
+++ source/uim/cms/controllers/actions/news/package.d
@@ -3,10 +3,4 @@ module uim.cms.controllers.actions.news;
 @safe:
 import uim.cms;
 
-// Main
-public import uim.cms.controllers.actions.news.action;
-
-// C(R)UD
-public import uim.cms.controllers.actions.news.create;
-public import uim.cms.controllers.actions.news.update;
-public import uim.cms.controllers.actions.news.delete_;
+mixin(EntityActions!("CMSNews", "/cms/news", "cms_news", "CMS"));
